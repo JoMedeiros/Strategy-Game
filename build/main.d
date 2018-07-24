416c2d64b0cf8bf0c671c8372b39f43ef13091c6
Breno Viana <brenomfviana@gmail.com>
2018-07-24 18:40:29 -0300

    Adding view, drawing map, implementing worker class

--- build/main.d
+++
@@ -1,8 +0,0 @@
-build/main.o: src/main.cpp include/game.hpp include/map.hpp \
- include/entity.hpp
-
-include/game.hpp:
-
-include/map.hpp:
-
-include/entity.hpp:
--- build/map.d
+++
@@ -1,5 +0,0 @@
-build/map.o: src/map.cpp include/map.hpp include/entity.hpp
-
-include/map.hpp:
-
-include/entity.hpp:
